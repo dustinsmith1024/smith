6b0057d307f621f5516ed3c04a26635606157d6d
Dustin Smith <dustinsmith1024@hotmail.com>
2011-05-17 22:22:29 -0500

    Pushing Rough Draft

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smith::Application.config.secret_token = '927fbae98fe27ec95e289c24427437a0de679314568153f9f7d39e7e959963d61ab52f4453f80c5cf855614a4933b7231173596e0f5645bfa508b4011616e5c6'
